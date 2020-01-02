3b7f491b896194b8c25814560932053a747dfc5a
Rutvik Patel <heyrutvik@gmail.com>
2020-01-02 18:31:41 +0530

    adds route for static files, and favicon file

--- project.clj
+++ project.clj
@@ -1,13 +1,14 @@
 (defproject roarer "0.1.0-SNAPSHOT"
   :description "An app to publish a thread on twitter without hassle."
-  :url "http://roarer.it"
+  :url "https://roarit.herokuapp.com/"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/tools.logging "0.5.0"]
                  [ring "1.8.0"]
                  [compojure "1.6.1"]
-                 [environ "1.1.0"]]
+                 [environ "1.1.0"]
+                 [clj-oauth "1.5.5"]]
   :min-lein-version "2.0.0"
   :uberjar-name "roarer.jar"
   :profiles {:production {:env {:production true}}})
--- src/roarer/core.clj
+++ src/roarer/core.clj
@@ -3,16 +3,21 @@
   (:require [ring.adapter.jetty :as jetty]
             [compojure.handler :refer [site]]
             [compojure.core :refer [defroutes GET]]
+            [compojure.route :as route]
             [environ.core :refer [env]]
-            [clojure.tools.logging :as log]))
+            [clojure.tools.logging :as log]
+            [ring.middleware.resource :refer [wrap-resource]]))
 
 (defroutes app
   (GET "/" []
     {:status 200
      :headers {"Content-Type" "text/plain"}
-     :body "Welcome to Roarer!"}))
+     :body "Welcome to Roarer!"})
+  (route/resources "/"))
 
 (defn -main [& [port]]
   (let [port (Integer. (or port (env :port) 8080))]
     (log/info (str "Starting the app at " port))
-    (jetty/run-jetty (site #'app) {:port port :join? false})))
+    (jetty/run-jetty
+      (-> (site #'app)
+          (wrap-resource "public")) {:port port :join? false})))
