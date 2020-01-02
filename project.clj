00b9e3e5ab165c78360012e671432c26965a493f
Rutvik Patel <heyrutvik@gmail.com>
2020-01-02 12:25:22 +0530

    Initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject roarer "0.1.0-SNAPSHOT"
+  :description "An app to publish a thread on twitter without hassle."
+  :url "http://roarer.it"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :main ^:skip-aot roarer.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ test/roarer/core_test.clj
@@ -0,0 +1,7 @@
+(ns roarer.core-test
+  (:require [clojure.test :refer :all]
+            [roarer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
