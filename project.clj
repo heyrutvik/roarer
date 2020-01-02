49c8a149210bbfd107457d163cebebad72cdeef6
Rutvik Patel <heyrutvik@gmail.com>
2020-01-02 16:32:58 +0530

    updates project file

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
                  [ring "1.8.0"]
                  [compojure "1.6.1"]
                  [environ "1.1.0"]]
-  :main ^:skip-aot roarer.core
+  :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.3.1"]]
+  :hooks [environ.leiningen.hooks]
   :uberjar-name "roarer.jar"
   :profiles {:production {:env {:production true}}})
