b4d63f9eee71dafd8b61397c3a760d481f5a4be1
Rutvik Patel <heyrutvik@gmail.com>
2020-01-02 15:16:36 +0530

    runs backend server on 8080

--- src/roarer/core.clj
+++ src/roarer/core.clj
@@ -1,7 +1,13 @@
 (ns roarer.core
-  (:gen-class))
+  (:gen-class)
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.util.response :as response]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn handler [request-map]
+  (response/response
+    "<html><body>Welcome to Roarer!</body></html>"))
+
+(defn -main [& args]
+  (jetty/run-jetty
+    handler
+    {:port 8080}))
